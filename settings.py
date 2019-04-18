1029a6e958f2262c875bc75ae62b46e8e8e5b8f0
Gyeongbae Jung <gyeongbae.jj@gmail.com>
2019-04-18 18:33:28 -0400

    edit settings to use heroku env vars

--- settings.py
+++ settings.py
@@ -1,9 +1,14 @@
-from environs import Env
+import os
 
+IS_PROD = os.environ.get('HEROKU', None)
 
-env = Env()
-
-
-NEO4J_HOST = env('NEO4J_HOST', default='localhost')
-NEO4J_USER = env('NEO4J_USER', default='neo4j')
-NEO4J_PASSWORD = env('NEO4J_PASSWORD', default='0529')
+if IS_PROD:
+    NEO4J_HOST = os.environ.get('NEO4J_HOST', None)
+    NEO4J_USER = os.environ.get('NEO4J_USER', None)
+    NEO4J_PASSWORD = os.environ.get('NEO4J_PASSWORD')
+else:
+    from environs import Env
+    env = Env()
+    NEO4J_HOST = env('NEO4J_HOST', default='localhost')
+    NEO4J_USER = env('NEO4J_USER', default='neo4j')
+    NEO4J_PASSWORD = env('NEO4J_PASSWORD', default='0529')
